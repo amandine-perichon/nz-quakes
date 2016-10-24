81ae6ec7860c5e201bd49a1ed96067efd07bb7ec
Amandine Perichon <apericho@gmail.com>
2016-10-24 21:24:10 +1300

    Add initial data

---
+++ download-quakes.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+wget 'http://quakesearch.geonet.org.nz/geojson?bbox=163.60840,-49.18170,182.98828,-32.28713&startdate=2015-11-01&enddate=2016-10-24'
+wget 'http://quakesearch.geonet.org.nz/geojson?bbox=163.60840,-49.18170,182.98828,-32.28713&startdate=2014-11-01&enddate=2015-11-01'
+wget 'http://quakesearch.geonet.org.nz/geojson?bbox=163.60840,-49.18170,182.98828,-32.28713&startdate=2013-11-01&enddate=2014-11-01'
+wget 'http://quakesearch.geonet.org.nz/geojson?bbox=163.60840,-49.18170,182.98828,-32.28713&startdate=2012-12-01&enddate=2013-11-01'
+wget 'http://quakesearch.geonet.org.nz/geojson?bbox=163.60840,-49.18170,182.98828,-32.28713&startdate=2011-11-01&enddate=2012-12-01'
+echo "Done 5"
+wget 'http://quakesearch.geonet.org.nz/geojson?bbox=163.60840,-49.18170,182.98828,-32.28713&startdate=2011-01-01&enddate=2011-11-01'
+wget 'http://quakesearch.geonet.org.nz/geojson?bbox=163.60840,-49.18170,182.98828,-32.28713&startdate=2010-03-01&enddate=2011-01-01'
+wget 'http://quakesearch.geonet.org.nz/geojson?bbox=163.60840,-49.18170,182.98828,-32.28713&startdate=2009-04-01&enddate=2010-03-01'
+wget 'http://quakesearch.geonet.org.nz/geojson?bbox=163.60840,-49.18170,182.98828,-32.28713&startdate=2008-04-01&enddate=2009-04-01'
+wget 'http://quakesearch.geonet.org.nz/geojson?bbox=163.60840,-49.18170,182.98828,-32.28713&startdate=2007-03-01&enddate=2008-04-01'
+echo "Done 10"
+wget 'http://quakesearch.geonet.org.nz/geojson?bbox=163.60840,-49.18170,182.98828,-32.28713&startdate=2005-12-01&enddate=2007-03-01'
+wget 'http://quakesearch.geonet.org.nz/geojson?bbox=163.60840,-49.18170,182.98828,-32.28713&startdate=2004-12-01&enddate=2005-12-01'
+wget 'http://quakesearch.geonet.org.nz/geojson?bbox=163.60840,-49.18170,182.98828,-32.28713&startdate=2003-11-01&enddate=2004-12-01'
+wget 'http://quakesearch.geonet.org.nz/geojson?bbox=163.60840,-49.18170,182.98828,-32.28713&startdate=2002-09-01&enddate=2003-11-01'
+wget 'http://quakesearch.geonet.org.nz/geojson?bbox=163.60840,-49.18170,182.98828,-32.28713&startdate=2001-04-01&enddate=2002-09-01'
+echo "Done 15"
+wget 'http://quakesearch.geonet.org.nz/geojson?bbox=163.60840,-49.18170,182.98828,-32.28713&startdate=1999-10-01&enddate=2001-04-01'
+wget 'http://quakesearch.geonet.org.nz/geojson?bbox=163.60840,-49.18170,182.98828,-32.28713&startdate=1998-01-01&enddate=1999-10-01'
+wget 'http://quakesearch.geonet.org.nz/geojson?bbox=163.60840,-49.18170,182.98828,-32.28713&startdate=1996-08-01&enddate=1998-01-01'
+wget 'http://quakesearch.geonet.org.nz/geojson?bbox=163.60840,-49.18170,182.98828,-32.28713&startdate=1995-03-01&enddate=1996-08-01'
+wget 'http://quakesearch.geonet.org.nz/geojson?bbox=163.60840,-49.18170,182.98828,-32.28713&startdate=1994-06-01&enddate=1995-03-01'
+echo "Done 20"
+wget 'http://quakesearch.geonet.org.nz/geojson?bbox=163.60840,-49.18170,182.98828,-32.28713&startdate=1993-04-01&enddate=1994-06-01'
+wget 'http://quakesearch.geonet.org.nz/geojson?bbox=163.60840,-49.18170,182.98828,-32.28713&startdate=1991-09-01&enddate=1993-04-01'
+wget 'http://quakesearch.geonet.org.nz/geojson?bbox=163.60840,-49.18170,182.98828,-32.28713&startdate=1990-02-01&enddate=1991-09-01'
+wget 'http://quakesearch.geonet.org.nz/geojson?bbox=163.60840,-49.18170,182.98828,-32.28713&startdate=1986-10-01&enddate=1990-02-01'
+wget 'http://quakesearch.geonet.org.nz/geojson?bbox=163.60840,-49.18170,182.98828,-32.28713&startdate=1985-9-24T7:00:00UTC&enddate=1986-10-01'
+echo "Done all"
